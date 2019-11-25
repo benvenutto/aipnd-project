68dd754e42d8480a280bf444d2309f7e0d301542
Simon Benvenutto <simon@benvenutto.com>
2019-11-25 16:43:48 +0000

    refactor model save/load to make fully stateless

--- util/state.py
+++ util/state.py
@@ -7,7 +7,7 @@ def save_snapshot(arch, model, optimiser, image_size, epoch, class_to_idx):
     snapshot = {
         'architecture': arch,
         'model': model.state_dict(),
-        'classifier_features': model._classifier_features,
+        'classifier_parameters': model._classifier_parameters,
         'image_size': image_size,
         'optimiser': optimiser.state_dict(),
         'epoch': epoch,
@@ -18,7 +18,7 @@ def save_snapshot(arch, model, optimiser, image_size, epoch, class_to_idx):
 def load_snapshot(checkpoint_file, device):
     snapshot = torch.load(checkpoint_file, map_location=device)
     arch = snapshot['arch']
-    classifier_params = snapshot[net.CLASSIFIER_PARAMETERS]
+    classifier_params = snapshot['classifier_parameters']
     model = net.make_model(arch, **classifier_params)
     model.load_state_dict(snapshot['model'])
     class_to_index = snapshot['class_to_idx']
